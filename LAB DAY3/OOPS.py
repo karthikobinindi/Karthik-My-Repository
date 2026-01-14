44a1274b1c1eee4195b2eac47e59f9df883450d9
karthikobinindi <obinindikarthik@gmail.com>
2026-01-14 16:17:57 +0530

    Add files via upload

---
+++ LAB DAY3/OOPS.py
@@ -0,0 +1,13 @@
+class student:
+    name = "Karthik"
+    age = 22
+s1= student()
+print(s1.name)
+print(s1.age)
+# Constructor
+class Employee:
+    def __init__(self,name,age):
+        self.name = "Karthik"
+        self.age = 22
+e1 = Employee("Karthik",22)
+print(e1.name,e1.age)
