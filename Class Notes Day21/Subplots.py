3d050ead8d95073535396b4812b599abe2ef0cb3
karthikobinindi <obinindikarthik@gmail.com>
2026-02-11 12:25:39 +0530

    Add files via upload

---
+++ Class Notes Day21/Subplots.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+plt.subplot(1, 2, 1)
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.title("plot1")
+
+plt.subplot(1, 2, 2)
+plt.bar(["a", "b", "c"], [3, 5, 7])
+plt.title("plot2")
+
+plt.savefig("report.png")
+
+plt.show()
